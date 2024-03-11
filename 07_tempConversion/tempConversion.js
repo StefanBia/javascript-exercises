d59b747f353741c6f53203cde928ef6ee447a59b
Stefan Bia <stefanbia0108@gmail.com>
2024-03-11 11:04:52 +0200

    Solve ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let cel = ((fahr - 32) * 5) / 9;
+  if(cel === Math.floor(cel))
+    return cel;
+  else
+    return parseFloat(cel.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+
+  let fahr = ((cel * 9)/5) + 32;
+  if(fahr === Math.floor(fahr))
+    return fahr;
+  else
+    return parseFloat(fahr.toFixed(1));
+
 };
 
 // Do not edit below this line
