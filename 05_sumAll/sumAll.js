af76b7630f363c891d1e92effa935d497ccf708e
Stefan Bia <stefanbia0108@gmail.com>
2024-03-11 10:50:16 +0200

    Solve ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(smallNumber, largeNumber) {
+
+    if(typeof smallNumber !== 'number' || typeof largeNumber !== 'number')
+        return 'ERROR';
+
+    if(smallNumber > largeNumber){
+        let aux = largeNumber;
+        largeNumber = smallNumber;
+        smallNumber = aux;
+    }
+
+    if(smallNumber < 0)
+        return 'ERROR';
+   
+
+    let result = 0;
+
+    for(i = smallNumber; i <= largeNumber; i++)
+        result += i;
+    return result;
 
 };
 
