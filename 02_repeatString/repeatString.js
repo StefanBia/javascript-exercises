6940919dd074ad262eee7777121465c3b7a7934e
Stefan Bia <stefanbia0108@gmail.com>
2024-03-11 10:25:58 +0200

    Solve ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberOfTimes) {
+    let result = "";
+    for(i = 0; i < numberOfTimes; i++){
+        result += stringToRepeat;
+    }
+    if(numberOfTimes < 0)
+        return 'ERROR';
+    return result;
 };
 
 // Do not edit below this line
