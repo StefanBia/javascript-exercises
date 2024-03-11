34564c267fb8e6901fe2108d661b5c71159bdaea
Stefan Bia <stefanbia0108@gmail.com>
2024-03-11 10:42:12 +0200

    Solve ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numbersToRemove) {
+    for (let numberToRemove of numbersToRemove) {
+        let indexesToRemove = [];
 
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === numberToRemove) {
+                indexesToRemove.push(i);
+            }
+        }
+
+        for (let i = indexesToRemove.length - 1; i >= 0; i--) {
+            arr.splice(indexesToRemove[i], 1);
+        }
+    }
+
+    return arr;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
