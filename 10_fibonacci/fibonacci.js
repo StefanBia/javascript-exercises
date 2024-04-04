23799d0a0218e91ef7b29e178a29c2eb76292b45
Stefan Bia <stefanbia0108@gmail.com>
2024-04-04 20:13:57 +0300

    Solve ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+  
+    if (typeof a === 'string') {
+        a = parseInt(a);
+    }
+    
+    if (a === 1 || a === 0) {
+        return a;
+    }
 
+    if (a < 0) {
+        return "OOPS";
+    }
+
+
+    let last = 1;
+    let secToLast = 0;
+    let result = 1;
+
+    for (let i = 2; i <= a; i++) {
+        result = last + secToLast;
+        secToLast = last;
+        last = result;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
