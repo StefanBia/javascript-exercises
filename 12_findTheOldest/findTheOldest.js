14b1b5e1e5dad55787ee3cd257252d6dd6450923
Stefan Bia <stefanbia0108@gmail.com>
2024-04-04 20:31:50 +0300

    Solve ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let oldest;
+    let maxAge = 0;
+    for(let i = 0; i < people.length; i++){
+
+        let birth, death;
+
+        birth = people[i].yearOfBirth;
+        death = people[i].yearOfDeath;
+
+        if(death === undefined){
+            death = 2024;
+        }
+
+        if(maxAge < death - birth){
+            maxAge = death - birth;
+            oldest = people[i];
+        }
+
+    }
+    return oldest;
 
 };
 
