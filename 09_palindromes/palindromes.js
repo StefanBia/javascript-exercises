33ddf9be4f2cedd86cd96af8d153d1394fdb2205
Stefan Bia <stefanbia0108@gmail.com>
2024-04-04 19:58:02 +0300

    Solve ex 9 with regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    string.toLowerCase();
+    string = string.toLowerCase().replace(/[^\w\d]/g, "").replace(/\s+/g, "");
+    for(let i = 0; i < string.length / 2; i++)
+        if(string.charAt(i) != string.charAt(string.length - 1 - i))
+            return false;
+
+    return true;
 
 };
 
