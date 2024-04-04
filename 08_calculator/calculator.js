6124768463ceb99e419fd430d18af47f539cebfa
Stefan Bia <stefanbia0108@gmail.com>
2024-04-04 19:49:07 +0300

    Solve ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,48 @@
-const add = function() {
+const add = function(a, b) {
+
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+
+  return arr.reduce((sum, current) => sum + current, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+
+  return arr.reduce((prod, current) => prod * current, 1);
 
 };
 
-const power = function() {
+const power = function(a, b) {
+
+    let pow = 1;
+
+    for(let i = 0; i < b; i++)
+      pow *= a;
+
+    return pow;
 	
 };
 
-const factorial = function() {
+const factorial = function(a) {
+
+  let fact = 1;
+    
+    for(let i = 1; i <= a; i++){
+       fact = fact * i;
+    }
+     
+
+    return fact;
 	
 };
 
